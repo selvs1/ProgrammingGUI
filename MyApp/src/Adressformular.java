223c084d4ea005f75cdd6bcc2dbc8faa2545d649
spsuji <42969019+spsuji@users.noreply.github.com>
2019-04-27 12:49:31 +0200

    GridPane basic

--- MyApp/src/Adressformular.java
+++ MyApp/src/Adressformular.java
@@ -1,21 +1,19 @@
 import javafx.application.Application;
-import javafx.scene.Parent;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Adressformular extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         GridPane root = new GridPane();
-        root.setGridLinesVisible(true);
+
 
         // Create GUI
 
@@ -41,32 +39,39 @@ public class Adressformular extends Application {
         Button buttonCancel = new Button("Cancel");
 
         // input in gridpane
-        root.add(lblHeadline, 0, 0);
+        root.add(lblHeadline, 0, 0, 4, 1);
 
-        root.add(lblName, 0, 1);
-        root.add(txtName, 1, 1);
+        root.add(lblName, 0, 1, 1, 1);
+        root.add(txtName, 1, 1, 4, 1);
 
-        root.add(lblVorname, 0, 2);
-        root.add(txtVorname, 1, 2);
+        root.add(lblVorname, 0, 2, 1, 1);
+        root.add(txtVorname, 1, 2, 4, 1);
 
-        root.add(lblStrasse, 0, 3);
-        root.add(txtStrasse, 1, 3);
+        root.add(lblStrasse, 0, 3, 1, 1);
+        root.add(txtStrasse, 1, 3, 4, 1);
 
-        root.add(lblPlz, 0, 4);
-        root.add(txtPlz, 1, 4);
-        root.add(lblOrt, 2, 4);
-        root.add(txtOrt, 3, 4);
+        root.add(lblPlz, 0, 4, 1, 1);
+        root.add(txtPlz, 1, 4, 1, 1);
+        root.add(lblOrt, 2, 4, 1, 1);
+        root.add(txtOrt, 3, 4, 1, 1);
 
-        root.add(buttonOk, 0, 5);
-        root.add(buttonCancel, 1, 5);
+        root.add(buttonOk, 2, 5, 1, 1);
+        root.add(buttonCancel, 3, 5, 1, 1);
+
+
+
+        // set properties
+        root.setGridLinesVisible(true);
+        root.setAlignment(Pos.CENTER);
+        GridPane.setHalignment(lblHeadline, HPos.RIGHT);
+        GridPane.setHalignment(buttonOk, HPos.RIGHT);
+        GridPane.setHalignment(buttonCancel, HPos.RIGHT);
 
 
         // Create GUI done
 
 
-
-
-        Scene scene = new Scene(root, 300,300);
+        Scene scene = new Scene(root, 500, 300);
         stage.setTitle("Adressformular");
         stage.setScene(scene);
         stage.show();
