276acc4d745abe83e5b5d2410f02536f0ed022d0
spsuji <42969019+spsuji@users.noreply.github.com>
2019-05-01 19:36:36 +0200

    Übung Stopwatch 2 fertig

--- MyApp/src/Stopwatch.java
+++ MyApp/src/Stopwatch.java
@@ -6,8 +6,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import java.lang.management.PlatformLoggingMXBean;
-
 
 public class Stopwatch extends BorderPane {
 
