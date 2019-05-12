541bf4b4f9b5b0330c5cf705755be381c0ea12f2
spsuji <42969019+spsuji@users.noreply.github.com>
2019-05-12 14:51:04 +0200

    GFR GUI zu 100% erstellt - mit Fehler und Anschiss

--- GFR/src/GFR.java
+++ GFR/src/GFR.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -40,7 +41,7 @@ public class GFR extends Application {
         lblMaleFemale = new Label("Geschlecht:");
         lblGfr = new Label("GFR:");
 
-        VBox column1 = new VBox();
+        VBox column1 = new VBox(10);
         column1.getChildren().addAll(lblAlter, lblWeight, lblConcentration, lblMaleFemale, lblGfr);
 
         // Rechte Spalte
@@ -61,19 +62,35 @@ public class GFR extends Application {
         lblGfrResult = new Label("getOutput");
 
         btnCalc = new Button("Berechnen");
+        btnCalc.addEventHandler(ActionEvent.ACTION, event -> {
+            // Formel ((140-Alter) * Gewicht) * [falls Weiblich: 0.85])/(72*[Serum Kreatinini in mg/dl])
+            int tmpAge = Integer.parseInt(cbAlter.getSelectionModel().getSelectedItem().toString());
+            //int tmp2 = (int) cbAlter.getSelectionModel().getSelectedItem();
+            double tmpWeight = Double.parseDouble(tfWeight.getText());
+            double tmpCreatinin = Double.parseDouble(tfConcentration.getText());
+            double tmpMfFactor = 1;
+            if (rbFemale.isSelected())
+                tmpMfFactor = 0.85;
 
+            double calcResult = ((140 - tmpAge) * tmpWeight) * (tmpMfFactor) / (72 * tmpCreatinin);
+
+            lblGfrResult.setText((String.format("%.2f", calcResult)));
+
+
+        });
         VBox column2 = new VBox();
         column2.getChildren().addAll(cbAlter, tfWeight, tfConcentration, rbMale, rbFemale, lblGfrResult);
 
 
-
-
         HBox topContent = new HBox();
         topContent.getChildren().addAll(column1, column2);
+        topContent.setStyle("-fx-border-color: black;" + "-fx-border-insets: 10;");
+
 
         HBox bottomContent = new HBox();
         bottomContent.getChildren().addAll(btnCalc);
         bottomContent.setAlignment(Pos.CENTER);
+        bottomContent.setStyle("-fx-border-color: red;");
 
         root.setCenter(topContent);
         root.setBottom(bottomContent);
@@ -81,12 +98,11 @@ public class GFR extends Application {
         // done
 
 
-        Scene scene = new Scene(root, 350, 300);
+        Scene scene = new Scene(root, 450, 500);
         stage.setTitle("Cockcroft-Gault-Formel");
         stage.setScene(scene);
         stage.show();
     }
-
 
 
     private Integer[] generator(int count) {
@@ -94,10 +110,11 @@ public class GFR extends Application {
         Integer[] result = new Integer[size];
 
         for (int i = 0; i < result.length; i++) {
-            result[i] = i+1;
+            result[i] = i + 1;
         }
         return result;
     }
+
 
     public static void main(String[] args) {
         launch(args);
